a22ccb1f32d3e5acc74fb3c4339bd4e04de65f32
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-07-01 11:51:47 +0200

    feat: routes for users

---
+++ src/routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+    getAllUsers,
+    createUser,
+} = require('../controllers/users')
+
+router.route('/').get(getAllUsers)
+router.route('/').post(createUser)
+
+module.exports = router 
