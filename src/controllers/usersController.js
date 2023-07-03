03a5ca55ea993212bcf474ecc74740915ffd730d
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-07-03 19:53:58 +0200

    fix: add catch part for get all users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -5,7 +5,8 @@ const getAllUsers = async (req, res) => {
         const users = await UserModel.find({}).select('username _id')
         res.status(200).json({ users })
     } catch (error) {
-        
+        console.log(error)
+        res.status(500).json({ error })
     }
 }
 
